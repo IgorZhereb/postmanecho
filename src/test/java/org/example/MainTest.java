9df6754bdf4cfe4d105bf7474d789d4f4176fb0a
igor <blackstanislav@gmail.com>
2023-05-23 20:18:49 +0500

    initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+class MainTest {
+    @Test
+    void shouldCheckAnswerOfRequest(){
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data") )
+        ;
+    }
+
+
+}
